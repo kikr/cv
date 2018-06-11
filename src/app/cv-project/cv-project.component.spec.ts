9ab512dfc852acdfb9e490f61a6e689d105dea22
kim <kimzero@hotmail.com>
2018-06-11 11:52:10 +0200

    Fixed tests

--- src/app/cv-project/cv-project.component.spec.ts
+++ src/app/cv-project/cv-project.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CvProjectComponent } from './cv-project.component';
+import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CvProjectComponent', () => {
   let component: CvProjectComponent;
@@ -8,7 +11,8 @@ describe('CvProjectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CvProjectComponent ]
+      declarations: [ CvProjectComponent ],
+      imports: [ MaterialModule, ReactiveFormsModule, RouterTestingModule ],
     })
     .compileComponents();
   }));
